837d885491bcccd95e1ccbdddc735776405d22d8
Karol Sójko <karolsojko@gmail.com>
2017-07-06 11:51:07 +0200

    Add CNAME for webpage

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -3,6 +3,7 @@ cd styleguide
 git init
 git config --global user.email "karolsojko@gmail.com"
 git config --global user.name "karolsojko"
+cp ../CNAME ./CNAME
 git add .
 git commit -m "Deployed to Github Pages"
 git push -f "https://github.com/x-team/unleash-styles" master:gh-pages

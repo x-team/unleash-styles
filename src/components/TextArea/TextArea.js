8bfd68f4d69f04ba641c019992d8807ff36da98d
Karol Sójko <karolsojko@gmail.com>
2017-08-11 12:00:26 +0200

    Change TextArea to a functional component

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styles from './TextArea.css'
 
 /**
  * TextArea
  */
-class TextArea extends Component {
-  render () {
-    return (
-      <textarea className={styles.TextArea} {...this.props} />
-    )
-  }
+function TextArea (props) {
+  return <textarea className={styles.TextArea} {...props} />
 }
 
 export default TextArea

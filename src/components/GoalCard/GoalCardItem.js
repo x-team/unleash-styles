a3e0333e8645c1007d34e19df3ecd443480389f0
Karol Sójko <karolsojko@gmail.com>
2017-08-25 13:16:39 +0200

    Add flow to GoalCard and GoalCardItem

--- src/components/GoalCard/GoalCardItem.js
+++ src/components/GoalCard/GoalCardItem.js
@@ -1,19 +1,24 @@
-import React from 'react'
+/** @flow */
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styles from './GoalCard.css'
 
 /**
  * GoalCardItem
  */
-function GoalCardItem (props) {
-  return (
-    <li className={styles.GoalCardItemCheckbox}>
-      <input className={styles.GoalCardItemInput} type='checkbox' id={props.label} checked={props.isComplete} />
-      <label className={styles.GoalCardItemLabel} for={props.label}>
-        {props.label}
-      </label>
-    </li>
-  )
+class GoalCardItem extends Component {
+  render () {
+    const { label, isComplete } = this.props
+
+    return (
+      <li className={styles.GoalCardItemCheckbox}>
+        <input className={styles.GoalCardItemInput} type='checkbox' id={label} checked={isComplete} />
+        <label className={styles.GoalCardItemLabel} for={label}>
+          {label}
+        </label>
+      </li>
+    )
+  }
 }
 
 GoalCardItem.propTypes = {

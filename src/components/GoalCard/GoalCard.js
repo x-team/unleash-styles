6ac34d930435f2cafc795debd4c305ba6a46facf
Karol Sójko <karolsojko@gmail.com>
2017-09-04 11:50:56 +0200

    Add expanded view of the GoalCard --closes #27 (#46)
    * Add expanded view of the GoalCard --closes #27
    
    * Add updating progress when GoalCardItem is toggled

--- src/components/GoalCard/GoalCard.js
+++ src/components/GoalCard/GoalCard.js
@@ -1,36 +1,77 @@
 /** @flow */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import styles from './GoalCard.css'
+import GoalCardItem from './GoalCardItem'
 
 /**
  * GoalCard
  */
 class GoalCard extends Component {
+  handleGoalCardItemToggleClick: Function
+
+  state: {
+    isOpen: boolean,
+    goalItemsCount: number,
+    goalItemsCompletedCount: number
+  }
+
+  constructor (props: Object) {
+    super(props)
+    this.state = {
+      isOpen: false,
+      goalItemsCount: props.children.length,
+      goalItemsCompletedCount: props.children.filter((item) => (item.props.isComplete)).length
+    }
+    this.handleGoalCardItemToggleClick = this.handleGoalCardItemToggleClick.bind(this)
+  }
+
+  handleGoalCardItemToggleClick (completed: boolean) {
+    const countDiff = completed ? 1 : -1
+    this.setState({
+      goalItemsCompletedCount: this.state.goalItemsCompletedCount + countDiff
+    })
+  }
+
+  handleSwitchViewClick () {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
   render () {
     const { title, onClick } = this.props
-    const goalItemsCount = this.props.children.length
-    const goalItemsCompletedCount = this.props.children.filter((item) => (item.props.isComplete)).length
+    const switchViewClasses = classNames(
+      styles.GoalCardSwitchView,
+      this.state.isOpen && styles.GoalCardSwitchViewIsOpen
+    )
+    const goalCardClasses = classNames(
+      styles.GoalCard,
+      this.state.isOpen && styles.GoalCardIsOpen
+    )
 
     return (
-      <li className={styles.GoalCard}>
+      <li className={goalCardClasses}>
         <div className={styles.GoalCardIntro}>
           <h3 className={styles.GoalCardTitle}>{title}</h3>
           <p className={styles.GoalCardDescription} />
           <div className={styles.GoalCardProgress}>
             <div className={styles.GoalCardSteps}>
               <span className={styles.GoalCardStepsLabel}>Steps:</span>
-              <span>{goalItemsCompletedCount}</span>
+              <span>{this.state.goalItemsCompletedCount}</span>
               <span className={styles.GoalCardStepsBar}>/</span>
-              <span>{goalItemsCount}</span>
+              <span>{this.state.goalItemsCount}</span>
             </div>
-            <progress className={styles.GoalCardProgressBar} max={goalItemsCount} value={goalItemsCompletedCount} />
-            <button className={styles.GoalCardSwitchView} />
+            <progress className={styles.GoalCardProgressBar} max={this.state.goalItemsCount} value={this.state.goalItemsCompletedCount} />
+            <button onClick={() => this.handleSwitchViewClick()} className={switchViewClasses} />
           </div>
         </div>
         <div className={styles.GoalCardInfo}>
           <ul className={styles.GoalCardChecklist}>
-            {this.props.children}
+            {this.props.children.map((goalCard, index) => (
+              <GoalCardItem key={index} onClick={this.handleGoalCardItemToggleClick} {...goalCard.props} />
+            ))}
             <li className={styles.GoalCardAddNew}>
               <button className={styles.GoalCardAddNewButton} onClick={onClick}>
                 <span className={styles.GoalCardAddNewButtonIcon}>+</span> add a new step
